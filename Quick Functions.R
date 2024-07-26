2dfff89d22b8d6bbfcb0e10a2eaad6d92f315ac5
Caio Fittipaldi Kenup <caio.kenup@gmail.com>
2024-07-26 18:58:12 +0100

    Update Quick Functions.R

--- Quick Functions.R
+++ Quick Functions.R
@@ -64,12 +64,12 @@ null2false<-function(x){if(length(x)==0){x<-FALSE};return(x)}
   	  }
 
 
-read_excel_allsheets <- function(filename, tibble = FALSE) {
+read_excel_allsheets <- function(y, tibble = FALSE) {
     # I prefer straight data.frames
     # but if you like tidyverse tibbles (the default with read_excel)
     # then just pass tibble = TRUE
-    sheets <- readxl::excel_sheets(filename)
-    x <- lapply(sheets, function(X) readxl::read_excel(filename, sheet = X))
+    sheets <- readxl::excel_sheets(y)
+    x <- lapply(sheets, function(X) readxl::read_excel(y, sheet = X))
     if(!tibble) x <- lapply(x, as.data.frame)
     names(x) <- sheets
     x
